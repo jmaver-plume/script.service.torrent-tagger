2d412a248f5ec70f81d630adfdd6f11d2a42a3f0
Jaka Maver <jm18457@gmail.com>
2023-01-29 18:57:42 +0100

    Fix linker bug and languages

--- service.py
+++ service.py
@@ -10,28 +10,30 @@ def main():
     addon = xbmcaddon.Addon()
     monitor = xbmc.Monitor()
 
-    i = 0
-    while not monitor.abortRequested():
-        if monitor.waitForAbort(10):
-            break
-        i += 10
-        if i == 300:
-            i = 0
-            xbmc.log(f"[{time.time()}]:  Running linker!", level=xbmc.LOGINFO)
-            utils = Utils(xbmc)
+    tv_shows_path = addon.getSetting('tv_shows_path')
+    movies_path = addon.getSetting('movies_path')
+    downloads_path = addon.getSetting('downloads_path')
+    downloads_state_path = addon.getSetting('downloads_state_path')
+    interval_in_seconds = int(addon.getSetting('interval_in_seconds'))
 
-            tv_shows_path = addon.getSetting('tv_shows_path')
-            movies_path = addon.getSetting('movies_path')
-            downloads_path = addon.getSetting('downloads_path')
-            downloads_state_path = addon.getSetting('downloads_state_path')
+    i = 0
+    tick = 10
+    while not monitor.abortRequested():
+        if monitor.waitForAbort(tick):
+            break
+        i += tick
+        if i >= interval_in_seconds:
+            i = 0
+            xbmc.log(f"[{time.time()}]:  Running linker!", level=xbmc.LOGDEBUG)
+            utils = Utils(xbmc)
 
             movie_scanner = MovieScanner(downloads_path, xbmc)
             tv_show_episode_scanner = TvShowEpisodeScanner(downloads_path, xbmc)
             tv_show_season_scanner = TvShowSeasonScanner(downloads_path, xbmc)
             downloads_directory = DownloadsDirectory(downloads_path, downloads_state_path, xbmc)
             movie_linker = MovieLinker(movies_path, movie_scanner, xbmc, utils)
-            episode_linker = TvShowLinker(movies_path, tv_show_episode_scanner, xbmc, utils)
-            season_linker = TvShowLinker(movies_path, tv_show_season_scanner, xbmc, utils)
+            episode_linker = TvShowLinker(tv_shows_path, tv_show_episode_scanner, xbmc, utils)
+            season_linker = TvShowLinker(tv_shows_path, tv_show_season_scanner, xbmc, utils)
 
             linker = Linker(
                 tv_shows_path=tv_shows_path,
