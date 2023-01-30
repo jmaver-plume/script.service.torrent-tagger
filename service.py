88381d5b23b855705a190993bb8f317287b3c31b
Jaka Maver <jmaver@plume.com>
2023-01-30 18:00:13 +0100

    refactor: Create a logger class for improved logging (#12)
    * refactor: Create a logger class for improved logging
    
    * Remove unused import

--- service.py
+++ service.py
@@ -1,9 +1,8 @@
-import time
 import xbmc
 import xbmcaddon
 
 from resources.lib.linker import Linker, MovieLinker, MovieScanner, TvShowLinker, TvShowEpisodeScanner, \
-    DownloadsDirectory, Utils, TvShowSeasonScanner
+    DownloadsDirectory, Utils, TvShowSeasonScanner, Logger
 
 
 def main():
@@ -16,6 +15,30 @@ def main():
     downloads_state_path = addon.getSetting('downloads_state_path')
     interval_in_seconds = int(addon.getSetting('interval_in_seconds'))
 
+    logger = Logger(xbmc)
+    utils = Utils(logger)
+    movie_scanner = MovieScanner(downloads_path, logger)
+    tv_show_episode_scanner = TvShowEpisodeScanner(downloads_path, logger)
+    tv_show_season_scanner = TvShowSeasonScanner(downloads_path, logger)
+    downloads_directory = DownloadsDirectory(downloads_path, downloads_state_path, logger)
+    movie_linker = MovieLinker(movies_path, movie_scanner, logger, utils)
+    episode_linker = TvShowLinker(tv_shows_path, tv_show_episode_scanner, logger, utils)
+    season_linker = TvShowLinker(tv_shows_path, tv_show_season_scanner, logger, utils)
+
+    linker = Linker(
+        tv_shows_path=tv_shows_path,
+        downloads_path=downloads_path,
+        movies_path=movies_path,
+        downloads_state_path=downloads_state_path,
+        xbmc=xbmc,
+        logger=logger,
+        movie_linker=movie_linker,
+        episode_linker=episode_linker,
+        season_linker=season_linker,
+        downloads_directory=downloads_directory,
+        utils=utils
+    )
+
     i = 0
     tick = 10
     while not monitor.abortRequested():
@@ -24,29 +47,7 @@ def main():
         i += tick
         if i >= interval_in_seconds:
             i = 0
-            xbmc.log(f"[{time.time()}]:  Running linker!", level=xbmc.LOGDEBUG)
-            utils = Utils(xbmc)
-
-            movie_scanner = MovieScanner(downloads_path, xbmc)
-            tv_show_episode_scanner = TvShowEpisodeScanner(downloads_path, xbmc)
-            tv_show_season_scanner = TvShowSeasonScanner(downloads_path, xbmc)
-            downloads_directory = DownloadsDirectory(downloads_path, downloads_state_path, xbmc)
-            movie_linker = MovieLinker(movies_path, movie_scanner, xbmc, utils)
-            episode_linker = TvShowLinker(tv_shows_path, tv_show_episode_scanner, xbmc, utils)
-            season_linker = TvShowLinker(tv_shows_path, tv_show_season_scanner, xbmc, utils)
-
-            linker = Linker(
-                tv_shows_path=tv_shows_path,
-                downloads_path=downloads_path,
-                movies_path=movies_path,
-                downloads_state_path=downloads_state_path,
-                xbmc=xbmc,
-                movie_linker=movie_linker,
-                episode_linker=episode_linker,
-                season_linker=season_linker,
-                downloads_directory=downloads_directory,
-                utils=utils
-            )
+            logger.debug('Running linker!')
             linker.link()
 
 
